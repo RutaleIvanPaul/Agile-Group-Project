21e77823ff01d9b2dac32a5efae68f6fc38be3ec
RutaleIvanPaul <paulivan568@gmail.com>
2018-06-01 11:09:43 -0400

    [Finishes #158024446]Create User Class template

---
+++ User.py
@@ -0,0 +1,26 @@
+class User:
+    userid = None 
+    username = None
+    password = None
+    lastlogin = None
+    level = None
+
+    def createAccount(username,password,level){
+        pass
+    }
+
+    def login(username,password){
+        pass
+    }
+
+    def createComment(message,timestamp,Author,parentcomment){
+        pass
+    }
+
+    def editComment(commentid){
+        pass
+    }
+
+    def logout(userid){
+        pass
+    }
